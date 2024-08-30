e202357be7c7c41bc7bb97c16f63f9e30800af29
SimanyeSTS <simanyesomdaka@gmail.com>
2024-08-30 11:17:38 +0200

    fetch single prediction done

--- model/Predictions.js
+++ model/Predictions.js
@@ -25,5 +25,30 @@ class Predictions {
             })
         }
     }
-    
+    fetchPrediction(req, res) {
+       try{
+        const strQry = `
+        SELECT P.predictionID, P.subcategoryID, S.subcategoryName, C.subcategoryName, C.categoryName, P.SOURCE, P.predictionDate, P.VALUE, P.bullishBearish
+        FROM Predictions P
+        JOIN Subcategories S
+        ON P.subcategoryID = S.subcategoryID
+        JOIN Category C
+        ON S.categoryID = C.categoryID
+        WHERE p.predictionID = ${req.params.ID};
+        `
+        db.query(strQry, (err, result) => {
+            if (err) throw new Error('Error fetching prediction.')
+                res.json({
+            status: res.statusCode,
+        result: result[0]
+    })
+        })
+    } catch (e) {
+        res.status(500).json({
+            status: 500,
+            msg: "Unable to fetch the prediction. Please try again later."
+        })
+    }
+       }
+
 }
