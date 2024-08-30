11e771c0c8036a0869e4e33b27a32d5466d052b3
SimanyeSTS <simanyesomdaka@gmail.com>
2024-08-30 14:13:02 +0200

    user routes setup

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,1 +1,35 @@
-///
+import express from 'express';
+import bodyParser from 'body-parser';
+import { users } from '../model/index';
+
+const userRouter = express.Router()
+userRouter.use(bodyParser.json())
+
+userRouter.get('/', (req, res) => {
+    users.fetchUsers(req,res)
+})
+
+userRouter.get('/:ID', (req, res) => {
+    users.fetchUser(req,res)
+})
+
+userRouter.post('/register', (req, res) => {
+    users.registerUser(req, res)
+})
+
+userRouter.patch('/:ID', (req, res) => {
+    users.updateUser(req, res)
+})
+
+userRouter.delete('/:ID', (req, res) => {
+    users.deleteUser(req, res)
+})
+
+userRouter.post('/login', (req, res) => {
+    users.loginUser(req, res)
+})
+
+export {
+    express,
+    userRouter
+}
