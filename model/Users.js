068028231cd184d3e4414b442c89c3c28ad412f1
SimanyeSTS <simanyesomdaka@gmail.com>
2024-08-30 10:36:49 +0200

    user model perfection

--- model/Users.js
+++ model/Users.js
@@ -4,175 +4,194 @@ import { compare, hash } from "bcrypt";
 
 class Users {
     fetchUsers(req, res) {
-        try {
-            const strQry = `
-            SELECT UserID, firstName, lastName, userAge, gender, userRole, emailAdd, userPass, userProfile, createdAt, updatedAt
-            FROM Users; 
-            `
-            db.query(strQry, (err, results) => {
-                if (err) throw new Error('Our apologies, a issue seems to have occured when retrieving the users. Please try again later.')
-                    res.json({
-                    status: res.statusCode,
-                    results
+        const strQry = `
+            SELECT UserID, firstName, lastName, userAge, gender, userRole, emailAdd, userProfile, createdAt, updatedAt
+            FROM Users;
+        `
+        db.query(strQry, (err, results) => {
+            if (err) {
+                return res.status(500).json({
+                    status: 500,
+                    msg: "An error occurred when retrieving users. Please try again later."
                 })
-            })
-        } catch (e) {
+            }
             res.json({
-                status: 404,
-                msg: "Please try again later"
+                status: 200,
+                results
             })
-        }
+        })
     }
+
     fetchUser(req, res) {
-        try {
-            const strQry = `
-            SELECT UserID, firstName, lastName, userAge, gender, emailAdd, userPass, userProfile, createdAt, updatedAt
+        const strQry = `
+            SELECT UserID, firstName, lastName, userAge, gender, emailAdd, userProfile, createdAt, updatedAt
             FROM Users
-            WHERE UserID = ${req.params.ID}
-            `
-            db.query(strQry, (err, result) => {
-                if (err) throw new Error('Our apologies, an issue seems to have occured whe retrieving this user. Please try again later.')
-                    res.json({
-                    status: res.statusCode,
-                    result: result[0]
+            WHERE UserID = ?;
+        `
+        db.query(strQry, [req.params.ID], (err, result) => {
+            if (err) {
+                return res.status(500).json({
+                    status: 500,
+                    msg: "An error occurred when retrieving the user. Please try again later."
                 })
-            })
-        } catch (e) {
+            }
+            if (!result.length) {
+                return res.status(404).json({
+                    status: 404,
+                    msg: "User not found."
+                })
+            }
             res.json({
-                status: 404,
-                msg: "Please try again later."
+                status: 200,
+                result: result[0]
             })
-        }
+        })
     }
+
     async registerUser(req, res) {
         try {
-            let data = req.body
-
-            data.userPass = await hash(data.userPass, 10)
-
-            let user = {
-                emailAdd: data.emailAdd,
-                userPass: data.userPass
-            }
+            let data = req.body;
+            data.userPass = await hash(data.userPass, 10);
 
             const strQry = `
             INSERT INTO Users
             SET ?;
             `
+
             db.query(strQry, [data], (err, results) => {
                 if (err) {
                     if (err.code === 'ER_DUP_ENTRY') {
                         return res.status(409).json({
-                            status: res.statusCode,
-                            msg: "This email appears to already be registered. Please log in or choose a different email address."
+                            status: 409,
+                            msg: "Email is already registered. Please log in or choose a different email."
                         })
-                    } else {
-                        return res.status(500).json({
-                            status: res.statusCode,
+                    }
+                    return res.status(500).json({
+                        status: 500,
                         msg: "An error occurred during registration. Please try again."
                     })
                 }
-            } else {
-                const token = createToken(user)
-                return res.json({
+                const token = createToken({ emailAdd: data.emailAdd });
+                res.json({
+                    status: 201,
                     token,
-                    msg: "You have been successfully registered to use FortuneTrack. Please log in to continue."
+                    msg: "Registration successful. Please log in to continue."
                 })
-            }
             })
         } catch (e) {
-            return res.status(404).json({
-                status: 404,
-                err: e.message
+            res.status(500).json({
+                status: 500,
+                msg: e.message
             })
         }
     }
+
     async updateUser(req, res) {
         try {
-            let data = req.body
+            let data = req.body;
             if (data.userPass) {
-                data.userPass = await hash(data.userPass, 10)
+                data.userPass = await hash(data.userPass, 10);
             }
             const strQry = `
             UPDATE Users
             SET ?
-            WHERE UserID = ${req.params.ID}
+            WHERE UserID = ?;
             `
-            db.query(strQry, [data], (err) => {
-                if (err) throw new Error('Our apologies, it seems we ran into an issue updating the user\'s information. Please try again later.')
-                    res.json({
-                status: res.statusCode,
-            msg: 'The information on this user\'s account has been successfully been updated.'
-        })
+            db.query(strQry, [data, req.params.ID], (err, result) => {
+                if (err) {
+                    return res.status(500).json({
+                        status: 500,
+                        msg: "Error updating user information. Please try again later."
+                    })
+                }
+                res.json({
+                    status: 200,
+                    msg: "User information successfully updated."
+                })
             })
         } catch (e) {
-            res.json({
-                status: 400,
-                err: e.message
+            res.status(500).json({
+                status: 500,
+                msg: e.message
             })
         }
     }
+
     deleteUser(req, res) {
-        try{
-            const strQry = `
+        const strQry = `
             DELETE FROM Users
-            WHERE UserID = ${req.params.ID};
-            `
-            db.query(strQry, (err) => {
-                if (err) throw new Error('Our apologies, the user couldn\'t be deleted. Please review your delete query to continue.')
-                    res.json({
-                status: res.statusCode,
-            msg: "Our apologies, the user couldn\'t be deleted. Please review your delete query to continue."})
+            WHERE UserID = ?;
+        `
+        db.query(strQry, [req.params.ID], (err, result) => {
+            if (err) {
+                return res.status(500).json({
+                    status: 500,
+                    msg: "Error deleting the user. Please try again later."
+                })
+            }
+            if (result.affectedRows === 0) {
+                return res.status(404).json({
+                    status: 404,
+                    msg: "User not found."
+                })
+            }
             res.json({
-                status: res.statusCode,
-                msg: "The user account has been successfully deleted."
+                status: 200,
+                msg: "User successfully deleted."
             })
-            })
-        } catch (e) {
-            res.json({
-                status: 404,
-                err: e.message
-            })
-        }
+        })
     }
+
     async login(req, res) {
         try {
-            const { emailAdd, userPass } = req.body
+            const { emailAdd, userPass } = req.body;
             const strQry = `
             SELECT UserID, firstName, lastName, userAge, gender, userRole, emailAdd, userPass, userProfile, createdAt, updatedAt
             FROM Users
-            WHERE emailAdd = ${emailAdd};
+            WHERE emailAdd = ?;
             `
-            db.query(strQry, [emailAdd], async (err, results) => {
-                if (err) throw new Error('Our apologies, we couldn\'t log you in. Please review your login query to continue.')
-                    if (!result?.length) {
-                        res.json({
-                            status: 401,
-                            msg: "It seems you have provided an invalid email not registered with FortuneTrack. Please register."
-                        })
-                    } else {
-                        const ValidPass = await compare(userPass, result[0].userPass)
-                        if (ValidPass) {
-                            const token = createToken({
-                                emailAdd,
-                                userPass
-                            })
-                            res.json({
-                                status: res.statusCode,
-                                token,
-                                result: result[0]
-                            })
-                        } else {
-                            res.json({
-                                status: 401,
-                                msg: "It seems you have provided an invalid password or have not registered with Aura Arstistry. Please try again."
-                            })
-                        }
+            db.query(strQry, [emailAdd], async (err, result) => {
+                if (err) {
+                    return res.status(500).json({
+                        status: 500,
+                        msg: "An error occurred while logging in. Please try again."
+                    })
+                }
+                if (!result.length) {
+                    return res.status(401).json({
+                        status: 401,
+                        msg: "Invalid email or password."
+                    })
+                }
+
+                const ValidPass = await compare(userPass, result[0].userPass);
+                if (!ValidPass) {
+                    return res.status(401).json({
+                        status: 401,
+                        msg: "Invalid email or password."
+                    })
+                }
+
+                const token = createToken({
+                    emailAdd: result[0].emailAdd,
+                    userPass: result[0].userPass
+                })
+
+                res.json({
+                    status: 200,
+                    token,
+                    user: {
+                        UserID: result[0].UserID,
+                        firstName: result[0].firstName,
+                        lastName: result[0].lastName,
+                        userRole: result[0].userRole,
+                        emailAdd: result[0].emailAdd
                     }
+                })
             })
         } catch (e) {
-            res.json({
-                status: 404,
+            res.status(500).json({
+                status: 500,
                 msg: e.message
             })
         }
@@ -180,5 +199,5 @@ class Users {
 }
 
 export {
-    Users
-}
+     Users 
+    }
