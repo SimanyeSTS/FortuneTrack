603bbf0160f05affc3e29388a05047b1961d8251
SimanyeSTS <simanyesomdaka@gmail.com>
2024-09-05 12:15:42 +0200

    Technology data is fetched from the api and stored to the database, changed schema to ALTER TABLE Technology MODIFY COLUMN SharesOutstanding BIGINT; to accomodate larger values on Technolgy

--- model/Technology.js
+++ model/Technology.js
@@ -2,15 +2,37 @@ import { connection as db } from "../config/index.js";
 
 class Technology {
   static async getTechnologyData() {
-    const query = 'SELECT * FROM Technology';
-    const [rows] = await db.query(query);
-    return rows;
+    try {
+      const query = `
+      SELECT * FROM Technology
+      `
+      const [rows] = await db.query(query)
+      return rows
+    } catch (error) {
+      console.error('Error fetching technoloogy data:', error)
+      throw error
+    }
   }
 
   static async saveTechnologyData(data) {
-    const query = 'INSERT INTO Technology SET ?';
-    await db.query(query, data);
+    try {
+      if (!data) {
+        throw new Error('Data cannot be null or undefined')
+      }
+
+      const columns = Object.keys(data)
+      const values = Object.values(data)
+      const placeholders = Array(columns.length).fill('?').join(', ')
+
+      const query = `
+      INSERT INTO Technology (${columns}) VALUES (${placeholders})
+      `
+      await db.query(query, values)
+    } catch (error) {
+      console.error('Error saving technology data:', error)
+      throw error
+    }
   }
 }
 
-export default Technology;
+export default Technology
