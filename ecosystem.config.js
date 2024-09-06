950b61b96ae956475ed21653ae3d0b6f3852985f
SimanyeSTS <simanyesomdaka@gmail.com>
2024-09-06 09:20:53 +0200

    tried implementing pm2 to run alongside nodemon for logs on cron but errors occured due to malformation (Forcing CJS to ESM), giving up as long as I am assured it works with just nodemon. I will checking my database to see if the updates do take place

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,30 +1,12 @@
-const cron = require('node-cron');
-const equityService = require('./services/AlphaVantage/equityService');
-const forexService = require('./services/AlphaVantage/forexService');
-const cryptoService = require('./services/AlphaVantage/cryptoService');
-const commodityService = require('./services/AlphaVantage/commodityService');
-
-cron.schedule('0 0 1 * *', async () => {
-  await equityService.fetchEquityData('AAPL');
-  await forexService.fetchForexData('EUR/USD');
-  await cryptoService.fetchCryptoData('BTC');
-  await commodityService.fetchCommodityData('ALL_COMMODITIES');
-})
-
-module.exports = {
+export default {
   apps: [
     {
-      name: 'fortune-track',
-      script: 'index.js',
-      instances: 1,
-      autorestart: true,
+      name: 'my-app',
+      script: 'nodemon',
+      args: ['--es-module', 'index.js'],
       watch: true,
-      max_memory_restart: '1G',
       env: {
-        NODE_ENV: 'development'
-      },
-      env_production: {
-        NODE_ENV: 'production'
+        NODE_ENV: 'development',
       }
     }
   ]
