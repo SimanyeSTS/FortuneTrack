2e958a77f9ef3e36b4c1bdb4db16337904113693
SimanyeSTS <simanyesomdaka@gmail.com>
2024-09-09 16:45:01 +0200

    Done

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,19 +1,84 @@
 <template>
-  <div>
-    
+  <div class="about">
+    <h1>About</h1>
+    <section class="about-content">
+      <p>
+        I, Simanye Somdaka, the developer and brains behind FortuneTrack, welcome you to this cutting-edge predictive analytics platform, designed to empower businesses with actionable insights across key industry sectors. This web application leverages the power of Node.js, Vue.js, and MySQL to deliver a robust and user-friendly tool for monitoring and forecasting trends in Retail, Technology, Food & Beverages, and Healthcare.
+      </p>
+      <p>
+        <span>My Mission:</span> I aim to provide businesses with timely and accurate data-driven predictions to optimize their strategies and maximize their potential. By integrating real-time data from Google Trends and X (formerly Twitter), this platform offers a comprehensive view of market dynamics and customer behavior.
+      </p>
+    </section>
+    <br>
+    <section class="founder-image">
+      <h2>Meet The Man Behind The Idea</h2>
+      <img src="https://i.postimg.cc/G3qqjxw8/IMG-1675202806226.jpg" alt="Simanye Somdaka" />
+    </section>
   </div>
 </template>
 
 <script>
-
-  export default {
-
-  
-
-}
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.about {
+  color: #ffffff;
+  font-family: Arial, sans-serif;
+  background-color: #000080;
+}
 
+.about-content h1 {
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 900;
+  color: white;
+  font-size: 30px;
+}
 
+.about-content p {
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 900;
+  color: white;
+  font-size: 20px;
+  line-height: 1.5;
+  text-align: left;
+  padding: 1%;
+}
+
+.founder-image {
+  text-align: center;
+  padding-bottom: 2%;
+}
+
+.founder-image h2 {
+  font-size: 2em;
+  margin-bottom: 20px;
+}
+
+.founder-image img {
+  max-width: 100%;
+  height: auto;
+  border-radius: 5px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+h1{
+  padding-top: 1%;
+  padding-left: 1%;
+  font-family: 'Montserrat', sans-serif;
+    font-weight: 900;
+    color: white;
+    font-size: 30px;
+}
+h2{
+  font-family: 'Montserrat', sans-serif;
+    font-weight: 900;
+    color: white;
+    font-size: 30px;
+}
+span {
+  font-family: 'Montserrat', sans-serif;
+    font-weight: 900;
+    color: white;
+    font-size: 30px;
+}
 </style>
