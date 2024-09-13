9c14667b3afdf8c7f2bac58957131658cd299530
SimanyeSTS <simanyesomdaka@gmail.com>
2024-09-13 13:47:43 +0200

    error handling

--- controller/allSectorsController.js
+++ controller/allSectorsController.js
@@ -1,9 +1,8 @@
 import express from 'express';
 import { AllSectors } from '../model/AllSectors.js';
 
-const allSectorsRouter = express.Router();
+const allSectorsRouter = express.Router()
 
-// Existing route for fetching all sectors
 allSectorsRouter.get('/', async (req, res) => {
   try {
     const sectors = await AllSectors.getAll();
@@ -17,32 +16,31 @@ allSectorsRouter.get('/', async (req, res) => {
       status: 500,
       results: [],
       message: error.message,
-    });
+    })
   }
-});
+})
 
-// New route for fetching prediction by symbol
 allSectorsRouter.get('/:symbol', async (req, res) => {
   try {
     const { symbol } = req.params;
-    const prediction = await AllSectors.getBySymbol(symbol); // Make sure this method exists in your model
+    const prediction = await AllSectors.getBySymbol(symbol)
     if (prediction) {
       res.json({
         status: 200,
         result: prediction,
-      });
+      })
     } else {
       res.status(404).json({
         status: 404,
         message: 'Prediction not found for symbol: ' + symbol,
-      });
+      })
     }
   } catch (error) {
     res.status(500).json({
       status: 500,
       message: error.message,
-    });
+    })
   }
-});
+})
 
-export { allSectorsRouter };
+export { allSectorsRouter }
